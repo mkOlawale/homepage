503e5e198fadd505d65322df9e1c0c3389696e91
mkOlawale <abeebolaking1@gmail.com>
2022-06-27 12:35:53 -0700

    newly

---
+++ inc/sendEmail.php
@@ -0,0 +1,70 @@
+﻿﻿<?php
+
+// Replace this with your own email address
+$siteOwnersEmail = 'hackerslord96@gmail.com';
+
+
+if($_POST) {
+
+   $name = trim(stripslashes($_POST['contactName']));
+   $email = trim(stripslashes($_POST['contactEmail']));
+   $subject = trim(stripslashes($_POST['contactSubject']));
+   $contact_message = trim(stripslashes($_POST['contactMessage']));
+
+   // Check Name
+	if (strlen($name) < 2) {
+		$error['name'] = "Please enter your name.";
+	}
+	// Check Email
+	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+		$error['email'] = "Please enter a valid email address.";
+	}
+	// Check Message
+	if (strlen($contact_message) < 15) {
+		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+	}
+   // Subject
+	if ($subject == '') { $subject = "Contact Form Submission"; }
+
+
+   // Set Message
+   $message = "";
+   $message .= "Email from: " . $name . "\n";
+	$message .= "Email address: " . $email . "\n";
+   $message .= "Message: \n";
+   $message .= $contact_message;
+   $message .= "\n ----- \n This email was sent from your site's contact form. <br />";
+
+   // Set From: header
+   $from =  $name . " <mail.sulaimanadewale.online>";
+
+   // Email Headers
+	$headers = "From: " . $from . PHP_EOL;
+	$headers .= "Reply-To: ". $email . PHP_EOL;
+ 	
+	
+
+
+   if (!@$error) {
+
+      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+
+		if ($mail) { echo "OK"; }
+      else { echo "Something went wrong. Please try again."; }
+		
+	} # end if - no validation error
+
+	else {
+
+		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
+		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
+		
+		echo $response;
+
+	} # end if - there was a validation error
+
+}
+
+?>
